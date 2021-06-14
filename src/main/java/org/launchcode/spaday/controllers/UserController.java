8b473e0401e681220eaac95bacebcb700d81198f
Christina Varghese <46170618+christinichka@users.noreply.github.com>
2021-06-14 13:37:56 -0500

    Completed 12.6.4 part 1. User sign up form rendering at user/add but not yet loading on submit.

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,8 +1,12 @@
 package org.launchcode.spaday.controllers;
 
 
+import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,9 +14,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     // user/add
-    @GetMapping
+    @GetMapping("add")
     public String displayAddUserForm() {
         return "/user/add"; // return the path to add.html
+    }
+
+    @PostMapping
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        if (user.getPassword().equals(verify)) {
+            return "user/index";
+        }
+        else {
+            model.addAttribute("error", true);
+            model.addAttribute("username", user.getUsername());
+            model.addAttribute("email", user.getEmail());
+
+            return "user/add";
+        }
     }
 
 }
